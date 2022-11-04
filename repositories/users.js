7235b227057da22ab52d6e49eb27c2450074e360
talli0505@naver.com <talli0505@naver.com>
2022-11-04 16:02:23 +0900

    2022-11-04 PM16:04 폴더 추가 조민수

---
+++ repositories/users.js
@@ -0,0 +1,46 @@
+const { Users } = require("../models");   
+const { Op } = require("sequelize");
+
+class UsersRepository { 
+
+  // 회원가입을 위한 함수
+  signUp = async (email, nickname, password, salt) => {
+
+    // create로 회원가입
+    const createAccountData = await Users.create({
+      email,
+      nickname,
+      password,
+      salt
+    });
+    return createAccountData;
+  };
+
+  // 유저 정보 조회 by 이메일과 닉네임을 위한 함수
+  findUserAccount = async (email, nickname) => {
+
+    // findOne로 email, nickname으로 이루어진 정보가 있는지 확인
+    const findUserAccountData = await Users.findOne({
+      where: {
+        [Op.or]: [{ email }, { nickname }],
+      },
+    });
+    return findUserAccountData;
+  };
+
+  // 로그인을 위한 함수
+  login = async (email) => {
+    
+    // findOne으로 email이 있는지 확인
+    const loginData = await Users.findOne({ where: {email} });
+    return loginData;
+  };
+
+  // refreshToken 업데이트 하는 함수
+  updateToken = async (email, refresh_token) => {
+    const updateTokenData = await Users.update({ refresh_token : refresh_token }, { where: { email : email } });
+    return updateTokenData;
+  }
+}
+
+module.exports = UsersRepository;
---
+++ routes/posts.router.js
@@ -0,0 +1,15 @@
+// const express = require('express');
+// const router = express.Router();
+
+// const authMiddleware = require("../middleware/auth-middleware");
+// const PostsController =require("../controller/posts");
+// const postsController = new PostsController();
+
+// router.post("/",postsController.createProduct);
+// router.get("/", postsController.findAllProduct);
+// router.get("/:postId", postsController.findOneProduct);
+// // router.put("/:postId",authMiddleware, postsController.updateProduct);
+// router.delete("/:postId", postsController.deleteProduct)
+
+
+// module.exports = router;
