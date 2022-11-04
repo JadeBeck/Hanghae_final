97a30ebf9be15038043bf34806381ba70063f691
talli0505@naver.com <talli0505@naver.com>
2022-11-04 22:06:56 +0900

    2022-11-04 PM22:06 스웨거 초본 제작 조민수

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 
-// const swaggerFile = require("./swagger-output");
-// const swaggerUi = require("swagger-ui-express");
-// const cors = require("cors");
+const swaggerFile = require("./swagger-output");
+const swaggerUi = require("swagger-ui-express");
+const cors = require("cors");
 
 app.use(express.json());
-// app.use(cors({
-// origin: '*', // 모든 출처 허용 옵션. true 를 써도 된다.
-// }));
+app.use(cors({
+origin: '*', // 모든 출처 허용 옵션. true 를 써도 된다.
+}));
 
-// app.use(
-//   "/swagger",
-//   swaggerUi.serve,
-//   swaggerUi.setup(swaggerFile, { explorer: true })
-// );
+app.use(
+  "/swagger",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerFile, { explorer: true })
+);
 
 app.use("/", routes);
 
